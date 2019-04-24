5382d358934c3016cb7be49dd7adddedf4817a74
黄晶焰 <wwwxuxiaojing@sina.com>
2019-04-24 22:22:04 +0800

    #redis learning

---
+++ learn_redis/src/main/java/com/jing/redis/controller/RedisController.java
@@ -0,0 +1,8 @@
+package com.jing.redis.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RedisController {
+
+}
---
+++ learn_redis/src/test/java/com/jing/redis/LearnRedisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jing.redis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearnRedisApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
